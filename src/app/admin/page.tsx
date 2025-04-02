4bb0419623cc07ee649be5fec01961e12bef8be6
mikelambson <macmicool@gmail.com>
2025-04-02 14:17:34 -0700

    ts build fixes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,19 +1,10 @@
 "use client";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
 import UploadSchedule from "@/features/schedule/uploadSchedule";
 
 const AdminPage = () => {
-    const router = useRouter();
+    
     return (
         <div className="flex flex-col">
-        {/* <nav className="admin-navbar">
-                <ul className="static top-0 left-0 pl-10 flex gap-4 z-50">
-                    <li><Link href="/admin/dashboard">Dashboard</Link></li>
-                    <li><Link href="/admin/users">Users</Link></li>
-                    <li><Link href="/admin/settings">Settings</Link></li>
-                </ul>
-        </nav> */}
         <div style={{ padding: '20px' }}>
             <h1>Admin Dashboard</h1>
             <p>Welcome to the admin panel. Use the navigation to manage the application.</p>
